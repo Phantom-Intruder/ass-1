2a6d6aab163e537b31e65d645caf6bb4511898d0
Mewantha B <Phantom Intruder>
2018-11-01 19:41:37 +0530

    corrected category issue

--- application/models/Category.php
+++ application/models/Category.php
@@ -63,4 +63,28 @@ class Category extends My_Model {
             $this->$key = $value;
         }
     }
+
+    /**
+     * Retrieve array of category models.
+     *
+     * @param int $limit
+     * @param int $offset
+     * @return array of category models from db, key is PK.
+     */
+    public function get($limit = 0, $offset = 0){
+        if ($limit){
+            $query = $this->db->get($this::DB_TABLE_NAME, $limit, $offset);
+        }
+        else{
+            $query = $this->db->get($this::DB_TABLE_NAME);
+        }
+        $ret_value = array();
+        $class = get_class($this);
+        foreach ($query->result() as $row){
+            $model = new $class;
+            $model->populate($row);
+            $ret_value[$row->{$this::DB_TABLE_PK_VALUE}] = $model;
+        }
+        return $ret_value;
+    }
 }
